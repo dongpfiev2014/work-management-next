9f5aa7717ca7e3df9df75b334f337aa3db357b25
dongpfiev2014 <dongpfiev2014@gmail.com>
2024-07-26 21:12:36 +0700

    hope final

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,12 +74,12 @@ export default function RootLayout({
           </Suspense>
         </StoreProvider>
       </body>
+      <GoogleAnalytics gaId="G-VKSXHFMYVP" />
       <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossOrigin="anonymous"
       />
-      <GoogleAnalytics gaId="G-VKSXHFMYVP" />
     </html>
   );
 }
